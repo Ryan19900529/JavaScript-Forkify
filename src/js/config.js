2e2cb336afa1529135a9ad5d2eea8531fbc4b930
Ryan <ab00999819@gmail.com>
2024-08-28 13:22:20 +0800

    finished

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '045de985-435c-4142-a03a-6639f93c0a71';
+export const MODAL_CLOSE_SEC = 2.5;
